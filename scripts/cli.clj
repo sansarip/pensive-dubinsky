d4d9c8252e2159957c113aca45eb12e85646af85
sepehr ansaripour <pehrans@gmail.com>
2021-02-06 11:31:28 -0500

    Make proper calles to parse, sort, and write records

--- scripts/cli.clj
+++ scripts/cli.clj
@@ -1,9 +1,41 @@
 (use 'pensive-dubinsky.parse)
+(use 'pensive-dubinsky.sort)
+(require '[clojure.string :as string])
 
-(parse-text
-  (reduce
-    (fn [s f] (str s (slurp f)))
-    ""
-    *command-line-args*))
+(def records
+  (parse-text
+    (reduce
+      (fn [s f] (str s (slurp f)))
+      ""
+      *command-line-args*)))
+
+(def time-ms (System/currentTimeMillis))
+
+(defn ->text [r]
+  (string/join
+    (System/lineSeparator)
+    (map #(string/join " | " (vals %)) r)))
+
+(def output-file-paths
+  {(str "out/sort-by-descending-email-and-ascending-last-name-"
+        time-ms
+        ".txt")
+   sort-by-descending-email-and-ascending-last-name
+   (str "out/sort-by-ascending-birth-date-"
+        time-ms
+        ".txt")
+   sort-by-ascending-birth-date
+   (str "out/sort-by-descending-last-name-"
+        time-ms
+        ".txt")
+   sort-by-descending-last-name})
+
+(doseq [[fp sort-fn] output-file-paths]
+  (spit fp ((comp ->text sort-fn) records)))
+
+
+
+
+
 
 
