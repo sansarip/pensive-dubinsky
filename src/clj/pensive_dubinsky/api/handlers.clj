7e179e7d7888114a907c2e9396f2b5775fd59273
sepehr ansaripour <pehrans@gmail.com>
2021-02-07 16:06:25 -0500

    Add handlers for required routes

--- src/clj/pensive_dubinsky/api/handlers.clj
+++ src/clj/pensive_dubinsky/api/handlers.clj
@@ -1,1 +1,25 @@
-(ns pensive-dubinsky.api.handlers)
+(ns pensive-dubinsky.api.handlers
+  (:require
+    [ring.util.http-response :as resp]
+    [pensive-dubinsky.api.db :as db]
+    [pensive-dubinsky.parse :as parse]
+    [pensive-dubinsky.sort :as sort*]))
+
+(defn sort-by-email [_]
+  (resp/ok
+    (sort*/sort-by-ascending-email @db/db)))
+
+(defn sort-by-full-name [_]
+  (resp/ok
+    (sort*/sort-by-ascending-full-name @db/db)))
+
+(defn sort-by-birth-date [_]
+  (resp/ok
+    (sort*/sort-by-ascending-birth-date @db/db)))
+
+(defn add-record [{{{data-line :data-line} :body} :parameters}]
+  (resp/created
+    ;; FIXME: Add correct location of resource
+    "/"
+    (db/add-record (parse/line->map data-line))))
+
