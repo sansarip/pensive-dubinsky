4d67750dafe518d1cef7c0408c8c091c644f2ab5
sepehr ansaripour <pehrans@gmail.com>
2021-02-07 16:06:07 -0500

    Add required routes

--- src/clj/pensive_dubinsky/api/routes.clj
+++ src/clj/pensive_dubinsky/api/routes.clj
@@ -1,1 +1,20 @@
-(ns pensive-dubinsky.api.routes)
+(ns pensive-dubinsky.api.routes
+  (:require
+    [pensive-dubinsky.api.handlers :as handlers]
+    [pensive-dubinsky.api.spec :as spec]))
+
+(def routes
+  ["/records"
+   [""
+    {:post {:handler    handlers/add-record
+            :parameters {:body {:data-line ::spec/data-line}}
+            :responses  {201 {:body ::spec/records}}}}]
+   ["/email"
+    {:get {:handler   handlers/sort-by-email
+           :responses {200 {:body ::spec/records}}}}]
+   ["/birthdate"
+    {:get {:handler   handlers/sort-by-birth-date
+           :responses {200 {:body ::spec/records}}}}]
+   ["/name"
+    {:get {:handler   handlers/sort-by-full-name
+           :responses {200 {:body ::spec/records}}}}]])
