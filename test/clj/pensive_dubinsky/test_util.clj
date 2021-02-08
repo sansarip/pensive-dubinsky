8578390cadc93591a41fa07874b85dbfa36f11da
sepehr ansaripour <pehrans@gmail.com>
2021-02-07 22:35:44 -0500

    Add cleanup test utility

--- test/clj/pensive_dubinsky/test_util.clj
+++ test/clj/pensive_dubinsky/test_util.clj
@@ -1,3 +1,14 @@
-(ns pensive-dubinsky.test-util)
+(ns pensive-dubinsky.test-util
+  (:require [pensive-dubinsky.api.db :as db]))
 
 (def num-tests 100)
+
+(defn with-cleanup [record f]
+  (try
+    (f)
+    (finally
+      (reset!
+        db/db
+        (filterv
+          (partial not= record)
+          @db/db)))))
