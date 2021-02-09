b4d1f824d3c82249a1e9d4c89f3eb663ef5354b2
sepehr ansaripour <pehrans@gmail.com>
2021-02-09 02:38:16 -0500

    Change behavior based on env and change binding to 0.0.0.0

--- src/clj/pensive_dubinsky/api/server.clj
+++ src/clj/pensive_dubinsky/api/server.clj
@@ -2,20 +2,23 @@
   (:require
     [io.pedestal.http :as server]
     [reitit.pedestal :as pedestal]
+    [pensive-dubinsky.api.env :as env]
     [pensive-dubinsky.api.interceptors :refer [reloadable-router router]]))
 
-(def service-map
-  (-> {::server/type            :jetty
-       ::server/port            3000
-       ::server/join?           false
-       ::server/routes          []
-       ::server/allowed-origins {:creds true :allowed-origins (constantly true)}
-       ::server/secure-headers  {:content-security-policy-settings {:object-src "'none'"}}}
-      (server/default-interceptors)
-      (pedestal/replace-last-interceptor reloadable-router)
-      (server/dev-interceptors)))
+(defonce service-map
+         (-> {::server/type            :jetty
+              ::server/host            "0.0.0.0"
+              ::server/port            3000
+              ::server/join?           false
+              ::server/routes          []
+              ;; FIXME: Restrict origins
+              ::server/allowed-origins {:creds true :allowed-origins (constantly true)}
+              ::server/secure-headers  {:content-security-policy-settings {:object-src "'none'"}}}
+             server/default-interceptors
+             (pedestal/replace-last-interceptor
+               ({:dev reloadable-router} env/env router))
+             (cond-> env/dev? server/dev-interceptors)))
 
-;; TODO: Create prod config
 (def server* (server/create-server service-map))
 
 (defn start! []
